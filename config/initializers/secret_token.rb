dfa22186750c8570d50bd3ea7a9f7b9aaf2a44ab
Edmond Yu <yukwunhang@gmail.com>
2013-02-20 19:10:53 -0800

    Using psql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Gottago::Application.config.secret_token = 'b9c3288b1539d2264227c9ebe10d6d135975b9e1ad284180cf2303b12901627f258cd1d87c13ca3980a7c708d982d1c3cf9cb00fcc641add97d3320759899c60'
+Gottago::Application.config.secret_token = 'ca47397e21a2ba52b7de0d1d54b9f4d3cf2dd5a10612c9cb277cfa6bbbf10cfe776af2f8cab9104faf49d9e167e1fad95fd30783b3f228d5de6849cb84dc188e'
