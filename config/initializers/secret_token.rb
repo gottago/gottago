527f8e22fba281dcc6ba4513bd1118882a9a6894
Edmond Yu <yukwunhang@gmail.com>
2013-02-20 18:35:06 -0800

    Initial commit; scaffolded

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gottago::Application.config.secret_token = 'b9c3288b1539d2264227c9ebe10d6d135975b9e1ad284180cf2303b12901627f258cd1d87c13ca3980a7c708d982d1c3cf9cb00fcc641add97d3320759899c60'
